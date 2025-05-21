701a219c3e305761dc3d6a334859cbd91491514d
Nishal Dinesh <nishaldinesh3516j@gmail.com>
2025-05-21 19:15:12 +0530

    search-part + like button yet to be added

--- clients/src/Pages/Home.jsx
+++ clients/src/Pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
     const navigate = useNavigate();
 
 useEffect(()=>{
-  if(currentUser){
+  if(!currentUser){
     navigate('/auth')
   }else{
     navigate('/')
